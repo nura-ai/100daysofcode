a2fb6eff91fa183ed4ca0a0577d39404cefe70c7
Eva <pipellala@gmail.com>
2025-08-17 14:06:27 +0200

    TASK FOR 7 DAY 7!🌟 YAY!

---
+++ day7_challange.py
@@ -0,0 +1,21 @@
+print(" Fake Fan Finder 🤔🧐")
+favAnime = input("What is your favorite anime? ")
+if favAnime == "Attack on Titan":
+    print("The world is cruel...")
+    Character = input("Name me any of the character? ")
+    if Character == "Eren Yeager":
+        print("Freedom is worth fighting for! You got that by pure chance. okay then, what did he say in 15th episode?")
+        quote = input("Type the quote: ")
+        if quote == "I don't want to die":
+            print("Correct!")
+        else:
+            print("See! Fake AoT fan!")
+    elif Character == "Mikasa Ackerman":
+        print("The strongest soldier! who she was always protecting?")
+        protector = input("Type the name: ")
+        if protector == "Eren Yeager":
+            print("Correct!")
+        else:
+            print("See! Fake AoT fan!")
+    else:
+        print("Interesting choice!")
