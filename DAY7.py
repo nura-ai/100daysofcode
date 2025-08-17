203edc8cccab6167ffe0cdc3a11655f38851304f
Eva <pipellala@gmail.com>
2025-08-17 14:05:47 +0200

    DAY 7 BABY!

---
+++ DAY7.py
@@ -0,0 +1,28 @@
+tvShow = input("what is your favorite TV show? ")
+if tvShow == "peppa pig":
+    print("ugh, why?")
+    faveCharacter = input("who is your favorite character? ")
+    if faveCharacter == "Peppa":
+        print("ugh, why?")
+    elif faveCharacter == "George":
+        print("he's a bit of a crybaby, isn't he?")
+    else:
+        print("interesting choice!")
+if tvShow == "Breaking Bad":
+    print("Ah, a person of taste!")
+    faveCharacter = input("who is your favorite character? ")
+    if faveCharacter == "Walter White":
+        print("I am not in danger, Skyler. I am the danger.")
+    elif faveCharacter == "Jesse Pinkman":
+        print("Yeah, science!")
+    else:
+        print("interesting choice!")
+if tvShow == "Game of Thrones":
+    print("Winter is coming!")
+    faveCharacter = input("who is your favorite character? ")
+    if faveCharacter == "Jon Snow":
+        print("You know nothing, Jon Snow.")
+    elif faveCharacter == "Daenerys Targaryen":
+        print("Dracarys!")
+    else:
+        print("interesting choice!")
