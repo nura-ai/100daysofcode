707fac31f9fd73d0d7b1c53a0b7764da0de851b2
Eva <pipellala@gmail.com>
2025-08-17 14:05:02 +0200

    finally day 6

---
+++ day6.py
@@ -0,0 +1,22 @@
+print("SECURE LOGIN")
+username = input("username > ")
+password = input("password > ")
+if username == "Luna":
+    print("Welcome, Luna! MY SUNSHINE!")
+elif username == "Lumi" and password == "54321":
+    print("Welcome, Lumi! MY darling!")
+elif username == "Rea" and password == "67890":
+    print("Welcome, Rea! MY funniest friend!🩵")
+elif username == "Evrika" and password == "13579":
+    print("Welcome, Evrika! MY brave one!🩵")
+elif username == "Luna Lumi" and password == "24680":
+    print("Welcome, Luna Lumi! MY shining star!🩵")
+elif username == "Kimi" and password == "11111":
+    print("Welcome, Kimi! MY kindheart!🌸")
+elif username == "Eva" and password == "12345":
+    print("Welcome, Eva! MY soul!🌸🫂🩵")
+elif username == "Iskra" and password == "99999":
+    print("Welcome, Iskra! MY clever one!🌙")
+else:
+    print("Go away, stranger! I don't know you!😡")
+    
