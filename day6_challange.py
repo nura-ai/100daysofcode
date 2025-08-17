3fe7ed98c959fb58b73c7f008192430401ec8c78
Eva <pipellala@gmail.com>
2025-08-17 14:05:23 +0200

    task for day 6

---
+++ day6_challange.py
@@ -0,0 +1,10 @@
+print("MY LOGIN SYSTEM")
+username = input("username > ")
+password = input("password > ")
+if username == "DIANA" and password == "12345":
+    print("Well, hello there Diana, what a lovely accent you have, you could have charmed your way in here even without a password!")
+    print("what brings you here to this humble abode?")
+    print("But since you did provide a password, let's make sure it's the right one...")
+    print("good job! you wrote your code perfectly!")
+else:
+    print("Welcome, stranger! I don't believe we've met before. So leave this place right now! 👺")
