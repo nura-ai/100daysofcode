d2033b0d3826062a9ad04aa6b697f2941c8e8a3d
Eva <pipellala@gmail.com>
2025-08-17 14:04:36 +0200

    again day 5

---
+++ day5_3.py
@@ -0,0 +1,17 @@
+print("_______Marvel Movie Character Creator_______")
+spiderMan = input("Do you you like hanging around?")
+if spiderMan.lower() == "yes":
+    print("Great! You would make a good Spider-Man.")
+else:
+    print("then you are not Spider-man")
+korg = input("do you have a 'gravelly' voice?")
+if korg.lower() == "yes":
+    print("Great! You would make a good Korg.")
+else:
+    print("aww, then you are not Korg.")
+blackWidow = input("are you a brave person?")
+if blackWidow.lower() == "yes":
+    print("Aha! you are Black Widow. You would make a good Black Widow. hi!")
+else:
+    print("aww, then you are not Black Widow.")
+    
