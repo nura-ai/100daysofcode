6803f306d2c2d63f69d595df77c7cb75f52e0bd2
Eva <pipellala@gmail.com>
2025-08-20 15:23:44 +0200

    d10

---
+++ marh-day10.py
@@ -0,0 +1,22 @@
+adding = 4 + 3
+print(adding)   
+
+substracting = 8 - 9
+print(substracting)
+
+multiplication = 4 * 32
+print(multiplication)
+
+division = 50 / 5
+print(division)
+
+squared = 4 ** 2
+print(squared)  
+
+modulo = 15 % 4
+print(modulo)
+
+divisor = 15 // 4
+print(divisor)
+
+
